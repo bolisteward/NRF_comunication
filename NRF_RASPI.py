83481a0743cb9e1a1443b3ea54d6a670772af2c1
bolisteward <bolisteward@users.noreply.github.com>
2021-02-10 17:00:32 -0500

    nrf24

--- NRF_RASPI.py
+++ NRF_RASPI.py
@@ -1,38 +1,53 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Example program to send packets to the radio link
+#
+
+
 import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+from lib_nrf24 import NRF24
 import time
 import spidev
-import NRF24
 
-#Begin the radio using GPIO08 as CE and GPIO25 as CSN pins.
-GPIO.setmode(GPIO.BCM)
 
-pipes = [[0xFF, 0xFF, 0xF1, 0xF1, 0xF2],[0xFF, 0xFF, 0xF2, 0xF2, 0xF3]]
 
-"""Set payload size as 32 bit, channel address as 76, data rate of 1 mbps and power levels as minimum."""
-radio.begin(0,25)
-radio.setPayloadSize(32)  
-radio.setChannel(0x76) 
-radio.setDataRate(NRF24.BR_1MBPS)    
+pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
+
+radio = NRF24(GPIO, spidev.SpiDev())
+radio.begin(0, 25)
+time.sleep(1)
+radio.setRetries(15,15)
+radio.setPayloadSize(32)
+radio.setChannel(0x60)
+
+radio.setDataRate(NRF24.BR_2MBPS)
 radio.setPALevel(NRF24.PA_MIN)
+radio.setAutoAck(True)
+radio.enableDynamicPayloads()
+radio.enableAckPayload()
 
-radio.openWritingPipe(pipes[0])     
+
+radio.openWritingPipe(pipes[1])
+radio.openReadingPipe(1, pipes[0])
 radio.printDetails()
 
-sendMessage = list("Hi..Arduino UNO")  
-while len(sendMessage) < 32:    
-    sendMessage.append(0)
 
+c=1
 while True:
-    start = time.time()      
-    radio.write(sendMessage)   
-    print("Sent the message: {}".format(sendMessage))  
-    radio.startListening() 
-
-while not radio.available(0):
-    time.sleep(1/100)
-    if time.time() - start > 2:
-        print("time out") # print error message if radio disconnected or not functioning anymore
-        break
-
-radio.stopListening()     # close radio
-time.sleep(3)  # give delay of 3 seconds
+    buf = ['H', 'E', 'L', 'O',c]
+    c = (c + 1) & 255
+    # send a packet to receiver
+    radio.write(buf)
+    print ("Sent:"),
+    print (buf)
+    # did it return with a payload?
+    if radio.isAckPayloadAvailable():
+        pl_buffer=[]
+        radio.read(pl_buffer, radio.getDynamicPayloadSize())
+        print ("Received back:"),
+        print (pl_buffer)
+    else:
+        print ("Received: Ack only, no payload")
+    time.sleep(10)
